201e5fe6b164a437b6ded1df18c97af18874f9b2
Gurbiggg <78986974+Gurbiggg@users.noreply.github.com>
2023-07-19 11:28:53 -0700

    Add files via upload

---
+++ code.py
@@ -0,0 +1,23 @@
+print("Starting")
+
+import board
+
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
+
+keyboard = KMKKeyboard()
+
+keyboard.col_pins = (board.D4, board.D5, board.D6,)
+keyboard.row_pins = (board.D7, board.D8, board.D9,)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
+
+keyboard.keymap = [
+    [KC.N1, KC.N2, KC.N3,
+     KC.N4, KC.N5, KC.N6,
+     KC.N7, KC.N8, KC.N9,
+    ]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
